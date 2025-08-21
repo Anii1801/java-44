a897cb74360fe131a66f4c1bfb5c6f9ba70e64f6
Anii1801 <anurudhs06@gmail.com>
2025-08-21 09:58:59 +0530

    inheritence

---
+++ oops/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CollegeStudent cs= new CollegeStudent("Ghaziabad",12);
+        cs.show_roll();
+        cs.show_address();
+    }
+}
