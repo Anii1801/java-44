4d011639fa963e863a8059cb17be7d60c152eee1
Anii1801 <anurudhs06@gmail.com>
2025-08-28 09:42:55 +0530

    User Defined Exception

---
+++ oops/src/UserDefinedException.java
@@ -0,0 +1,9 @@
+// step-2
+public class UserDefinedException {
+    public void validate(int age) throws InvalidAgeException {
+        if(age<18)
+            throw new InvalidAgeException("Not Eligible to Vote age less than 18");
+        else
+            System.out.println("Eligible to Vote");
+    }
+}
